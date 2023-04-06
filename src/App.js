72b35e5eabf5a056cbf958deb59e943a00fdcfef
antudas22 <20antu20@gmail.com>
2023-04-06 19:21:37 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="">
+      <header>
+        <h1 className='text-3xl font-bold text-center mt-10'>Todo - List</h1>
       </header>
+      <div>
+
+      </div>
+      <div className='min-h-[600px] w-[500px] bg-secondary mx-auto mt-10 rounded-2xl'>
+
+      </div>
     </div>
   );
 }
